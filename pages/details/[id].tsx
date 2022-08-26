bc9bde55d19e0b2add84ac091f29f09f8797d0a9
김지호 <crsn1111@gmail.com>
2022-08-26 23:26:57 +0900

    [feat] 기능 추가

--- pages/details/[id].tsx
+++ pages/details/[id].tsx
@@ -1,6 +1,21 @@
-const Details = (prop:any) => {
-  console.log(prop)
-  return <div>?</div>;
+const Details = () => {
+  return (
+    <div className="w-full h-screen bg-blue-500 relative ">
+      <div className="w-12 h-screen bg-yellow-200 fixed"></div>
+      <div className="w-48 h-[calc(100vh-4rem)] bottom-0 bg-green-200 absolute left-12 overflow-y-scroll">
+        <span className="fixed text-2xl top-0 bg-red-500 h-16">hello</span>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map(
+          (data, i) => {
+            return (
+              <div className="w-full h-16 bg-red-300" key={i}>
+                {data}
+              </div>
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Details;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { Suspense, useState } from "react";
+import { useCookies } from "react-cookie";
 import { SubmitHandler, useForm } from "react-hook-form";
-import duck from "../public/duck.jpg";
+import Skeleton from "react-loading-skeleton";
+import Button from "../components/Button";
+import Input from "../components/Input";
 
 // 유저 프로필 검색 페이지
 
@@ -16,9 +19,11 @@ const Home: NextPage = () => {
   const [inputId, setInputId] = useState<String>("");
   const onValid: SubmitHandler<ProfileId> = ({ Id }) => {
     setInputId(Id);
+    setCookie("id", Id);
     setValue("Id", "");
   };
-  console.log(duck);
+  const [cookies, setCookie] = useCookies(["id"]);
+  console.log(cookies);
   return (
     <div className="w-full h-screen bg-blue-400">
       <Image
@@ -28,9 +33,10 @@ const Home: NextPage = () => {
         width={50}
         height={50}
       />
-      <div className="w-full text-center text-red-500 absolute">
+      <div className="w-full text-center text-gray-700 absolute">
         <form onSubmit={handleSubmit(onValid)}>
-          <input {...register("Id")} />
+          <Input register={register("Id", { required: true })} />
+          <Button text="안녕" type="submit" />
         </form>
         <Link href={`/details/${inputId}`}>{inputId}</Link>
       </div>
